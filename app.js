8dc32547ecdd28c18739f431c11f58955e637391
Jairus San Juan <jrscerda@gmail.com>
2022-04-24 13:36:21 +0800

    changed -s to -t to match flags

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const generate = require('./commands/generate')
 program
    .command('generate')
    .description('generate express files from yaml template')
-   .option('-s, --template <template>', 'template file to be used.')
+   .option('-t, --template <template>', 'template file to be used.')
    .action(generate)
 
 program.parse()
