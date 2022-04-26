b02ad7c961f75bd8ec88fff50283a4e933520798
Jairus San Juan <jrscerda@gmail.com>
2022-04-26 16:03:12 +0800

    added utility Generator class to methodify the process of writing files

--- classes/Generator.js
+++ classes/Generator.js
@@ -1,9 +1,28 @@
 /* eslint-disable */
 
-const { appendFile } = require('fs')
+const { writeFile } = require('fs')
 const process = require('process')
+const { entryFile } = require('../templates/index')
 
 const dir = process.cwd()
+
+function write(filePath, value) {
+   return new Promise((resolve, reject) => {
+      writeFile(
+         filePath,
+         value,
+         {
+            encoding: 'utf-8',
+            flag: 'w',
+         },
+         err => {
+            if (err) reject(err)
+
+            resolve()
+         }
+      )
+   })
+}
 
 class Generator {
    constructor({ routes, controllers }) {
@@ -11,13 +30,33 @@ class Generator {
       this.controllers = controllers
    }
 
-   generateBase() {}
+   checkRoutes() {
+      if (!this.routes) throw 'Template file - Routes Property not found.'
+   }
 
-   generateRoutes() {}
+   checkControllers() {
+      if (!this.controllers) throw 'Template file - Controllers Property not found.'
+   }
 
-   generateControllers() {}
+   async generateBase() {
+      try {
+         this.checkRoutes()
 
-   generate() {}
+         let content = entryFile(this.routes)
+
+         let path = dir + '/sample.js'
+
+         await write(path, content)
+      } catch (error) {
+         throw error
+      }
+   }
+
+   async generateRoutes() {}
+
+   async generateControllers() {}
+
+   async generate() {}
 }
 
 module.exports = Generator
