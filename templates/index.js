66f8f576b6fb35c5febfa4cd5875a6f1d33cc3ea
Jairus San Juan <jrscerda@gmail.com>
2022-05-14 13:20:03 +0800

    adds programmatic formatting of generated files via child-process

--- templates/index.js
+++ templates/index.js
@@ -23,13 +23,13 @@ let routeFile = function (HTTPMethods) {
 
    HTTPMethods.forEach(method => {
       if (allowedMethods.includes(method.toLowerCase()))
-         methodsUsed += `  .${method.toLowerCase()}((req, res, next) => {\n\t\tres.status(200).end()\n  })\n`
+         methodsUsed += `.${method.toLowerCase()}((req, res, next) => {\nres.status(200).end()\n})\n`
    })
 
    let template = [
       "const express = require('express')",
       'const router = express.Router()\n',
-      `router\n  .route('/')`,
+      `router\n.route('/')`,
       methodsUsed,
       'module.exports = router',
    ]
